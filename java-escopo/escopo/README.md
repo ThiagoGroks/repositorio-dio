bdb7c83c7438201d02bdca7e146c3a0be001f251
ThiagoGroks <thiago.groks@gmail.com>
2023-01-17 07:08:01 -0300

    Microwave fisrt steps.

---
+++ java-escopo/escopo/README.md
@@ -0,0 +1,5 @@
+# Escopo
+
+O escopo de uma variável é o ambiente onde ela pode ser acessada.
+
+É fundamental o domínio de escopo para programação.
---
+++ micro/microondas/src/Microwave.java
@@ -0,0 +1,12 @@
+public class Microwave {
+    boolean start = false;
+    double chicken = 5.5;
+    int meat = 10;
+    int popcorn = 4;
+    boolean pause = false;
+    boolean rest = true;
+
+    public void startChicken(){
+        System.out.println("Timer = " + chicken);
+    }
+}
