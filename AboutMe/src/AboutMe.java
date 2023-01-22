3d77828242efe0b0b437b15f67fe24d8a81f82f9
ThiagoGroks <thiago.groks@gmail.com>
2023-01-22 07:25:24 -0300

    Problmas com o app. Necessário mudar a organização

--- AboutMe/src/AboutMe.java
+++ AboutMe/src/AboutMe.java
@@ -1,12 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //criando o scanner;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //qual mensagem será lida, e que tipo de dado aceito?
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
         
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.println("Digite a sua idade");
+        int idade = scanner.nextInt();
         
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //impressão dos dados obtidos
+        System.out.println("Olá! Me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos.");
         System.out.println("Minha altura é " + altura + "cm.");
     }
