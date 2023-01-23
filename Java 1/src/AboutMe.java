ef40fc4d38c432d508afa2f13feb429e112d2853
ThiagoGroks <thiago.groks@gmail.com>
2023-01-23 06:44:41 -0300

    Alterações na pasta inicial

---
+++ Java 1/src/AboutMe.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        //criando o scanner;
+        Scanner scanner = new Scanner(System.in).userLocale(Locale.US);
+        //qual mensagem será lida, e que tipo de dado aceito?
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+        
+        System.out.println("Digite a sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //impressão dos dados obtidos
+        System.out.println("Olá! Me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Minha altura é " + altura + "cm.");
+    }
+}
